b95e51b7f56f28e3a90b602da8b3a44114dbaff2
Toru3 <ryuutet@gmail.com>
2022-10-14 10:30:07 +0900

    fmt修正

--- src/small.rs
+++ src/small.rs
@@ -1,2 +1,6 @@
-#![allow(unknown_lints, clippy::collapsible_else_if, clippy::bool_to_int_with_if)]
+#![allow(
+    unknown_lints,
+    clippy::collapsible_else_if,
+    clippy::bool_to_int_with_if
+)]
 include!(concat!(env!("OUT_DIR"), "/small.rs"));
